9a8a11d1557477bc4c3a719f2cd5dbab71ab8f54
none-life <task-discus-0a@icloud.com>
2022-12-13 16:38:28 +0100

    Initial commit

---
+++ CHANGELOG.md
@@ -0,0 +1,2 @@
+# 1.0.0
+Initial release, keep up with new released with links to issues or tasks
---
+++ registration.php
@@ -0,0 +1,3 @@
+<?php use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'VMPL_Base', __DIR__);
