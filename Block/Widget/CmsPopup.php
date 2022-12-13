47ef8dc51cbe11b8ead8a2ac4c2380be8654a095
none-life <task-discus-0a@icloud.com>
2022-12-13 17:55:48 +0100

    feat(ACPM-7) Adding new widget type with default template and class

---
+++ Block/Widget/CmsPopup.php
@@ -0,0 +1,82 @@
+<?php declare(strict_types=1);
+
+namespace VMPL\PopupManagement\Block\Widget;
+
+use Magento\Cms\Api\BlockRepositoryInterface;
+use Magento\Cms\Model\Block as CmsBlock;
+use Magento\Cms\Model\Template\FilterProvider;
+use Magento\Framework\DataObject\IdentityInterface;
+use Magento\Framework\Exception\NoSuchEntityException;
+use Magento\Framework\View\Element\Template\Context;
+use Magento\Widget\Block\BlockInterface;
+
+class CmsPopup extends \Magento\Framework\View\Element\Template implements BlockInterface, IdentityInterface
+{
+    protected static $widgetUsageMap = [];
+    protected ?CmsBlock $block = null;
+
+    public function __construct(
+        Context                                     $context,
+        protected readonly FilterProvider           $filterProvider,
+        protected readonly BlockRepositoryInterface $blockRepository,
+        array                                       $data = []
+    ) {
+        parent::__construct($context, $data);
+    }
+
+    protected function _beforeToHtml()
+    {
+        parent::_beforeToHtml();
+
+        $blockId = $this->getData('block_id');
+        $blockHash = get_class($this) . $blockId;
+
+        if (isset(self::$widgetUsageMap[$blockHash])) {
+            return $this;
+        }
+        self::$widgetUsageMap[$blockHash] = true;
+
+        $block = $this->getBlock();
+
+        if ($block && $block->isActive()) {
+            try {
+                $storeId = $this->getData('store_id') ?? $this->_storeManager->getStore()->getId();
+                $this->setText(
+                    $this->filterProvider->getBlockFilter()->setStoreId($storeId)->filter($block->getContent())
+                );
+            } catch (NoSuchEntityException $e) {
+            }
+        }
+        unset(self::$widgetUsageMap[$blockHash]);
+        return $this;
+    }
+
+    public function getIdentities()
+    {
+        $block = $this->getBlock();
+
+        if ($block) {
+            return $block->getIdentities();
+        }
+
+        return [];
+    }
+
+    private function getBlock(): ?CmsBlock
+    {
+        if (empty($this->block)) {
+            if (($blockId = $this->getData('block_id'))) {
+                try {
+                    /** @var CmsBlock $block */
+                    $block = $this->blockRepository->getById($blockId);
+                    if (in_array($this->_storeManager->getStore()->getId(), $block->getStores())) {
+                        $this->block = $block;
+                    }
+                } catch (NoSuchEntityException $e) {
+                }
+            }
+        }
+
+        return $this->block;
+    }
+}
