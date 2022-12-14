49eb5255037dd3628990a8ea8e8d03c14de27e52
none-life <task-discus-0a@icloud.com>
2022-12-14 18:08:39 +0100

    feat(ACPM-9) Adding store configuration options

---
+++ Type/Config.php
@@ -0,0 +1,12 @@
+<?php declare(strict_types=1);
+
+namespace VMPL\PopupManagement\Type;
+
+enum Config : string
+{
+    case Delay = 'cms/popup/delay';
+    case Timeout = 'cms/popup/timeout';
+    case Count = 'cms/popup/count';
+    case ClickableOverlay = 'cms/popup/clickableOverlay';
+    case PopupType = 'cms/popup/type';
+}
