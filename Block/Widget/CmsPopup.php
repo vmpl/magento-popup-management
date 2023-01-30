1ff8375e1a401b9f07e2a73078bd57220e3f2526
none-life <task-discus-0a@icloud.com>
2023-01-30 22:05:07 +0100

    fix(ACPM-17) Merge conflict

--- Block/Widget/CmsPopup.php
+++ Block/Widget/CmsPopup.php
@@ -8,6 +8,21 @@ use VMPL\PopupManagement\Helper\Config;
 
 class CmsPopup extends \Magento\Cms\Block\Widget\Block implements BlockInterface, IdentityInterface
 {
+    public function __construct(
+        \Magento\Framework\View\Element\Template\Context $context,
+        \Magento\Cms\Model\Template\FilterProvider       $filterProvider,
+        \Magento\Cms\Model\BlockFactory                  $blockFactory,
+        protected readonly Config                        $config,
+        array                                            $data = [],
+    ) {
+        parent::__construct(
+            $context,
+            $filterProvider,
+            $blockFactory,
+            $data
+        );
+    }
+
     /**
      * @return string
      */
