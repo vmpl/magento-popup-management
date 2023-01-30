324f844bd251f4d0a0755ce65a195be372d138d1
none-life <task-discus-0a@icloud.com>
2023-01-30 21:37:27 +0100

    fix(ACPM-9) Adding docs and remove old class

--- Block/Widget/CmsPopup.php
+++ Block/Widget/CmsPopup.php
@@ -5,6 +5,9 @@ namespace VMPL\PopupManagement\Block\Widget;
 use Magento\Framework\DataObject\IdentityInterface;
 use Magento\Widget\Block\BlockInterface;
 
+/**
+ * Block of the widget
+ */
 class CmsPopup extends \Magento\Cms\Block\Widget\Block implements BlockInterface, IdentityInterface
 {
     /**
--- Model/ConfigProvider.php
+++ Model/ConfigProvider.php
@@ -5,14 +5,25 @@ namespace VMPL\PopupManagement\Model;
 use Magento\Framework\App\Config\ScopeConfigInterface;
 use VMPL\PopupManagement\Type\Config as ConfigTypes;
 
+/**
+ * Class used to get store configuration preferences or other settings
+ */
 class ConfigProvider
 {
+    /**
+     * @param ScopeConfigInterface $scopeConfig
+     */
     public function __construct(
         protected readonly ScopeConfigInterface $scopeConfig
     ) {
     }
 
-    public function getConfig(ConfigTypes $type, $storeId = null)
+    /**
+     * @param ConfigTypes $type
+     * @param $storeId
+     * @return mixed
+     */
+    public function getConfig(ConfigTypes $type, $storeId = null): mixed
     {
         return $this->scopeConfig->getValue($type->value,
             \Magento\Store\Model\ScopeInterface::SCOPE_STORE,
