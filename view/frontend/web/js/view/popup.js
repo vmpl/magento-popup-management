667885c77e044d65dc7589e044bdc23ea5f0b189
Kamil Wiśniowski <kamil.wisniowski@vm.pl>
2023-04-17 23:04:40 +0200

    feat(ACPM-21) Adding new template with dialog element functionality

--- view/frontend/web/js/view/popup.js
+++ view/frontend/web/js/view/popup.js
@@ -2,33 +2,13 @@ define([
     'jquery',
     'uiComponent',
     'Magento_Ui/js/modal/modal',
-    'domReady!'
-], function ($, Component, modal) {
-    const storage = class {
-        #storageKey;
-        #elementKey;
-        #data = {};
-        constructor(storageKey, elementKey) {
-            this.#storageKey = storageKey;
-            this.#elementKey = elementKey;
-            this.#data = JSON.parse(localStorage.getItem(this.#storageKey) ?? '{}');
-        }
-
-        get count() {
-            return this.#data.hasOwnProperty(this.#elementKey)
-                ? ~~this.#data[this.#elementKey]
-                : 0;
-        }
-
-        set count(index) {
-            this.#data[this.#elementKey] = index;
-            localStorage.setItem(this.#storageKey, JSON.stringify(this.#data));
-        }
-    }
-
+    'VMPL_PopupManagement/js/lib/storage',
+    'domReady!',
+], function ($, Component, modal, storage) {
     return Component.extend({
         defaults: {
             waitTime: 3000,
+            closeTimeout: 0,
             limitOpened: 0,
             storageKey: 'vmpl__popup_displayed',
             modalSettings: {
@@ -41,18 +21,22 @@ define([
         initialize(config, element) {
             this._super(config, element);
             this.element = element;
-
-            const modalSettings = this.modalSettings;
-            modalSettings.opened = this.onOpened.bind(this)
-            this.popup = modal(modalSettings, $(this.element));
-
             if (!this.element.id) {
                 return console.warn(`Popup element is missing id attribute`);
             }
             this.storage = new storage(this.storageKey, this.element.id ?? 'default')
 
+            this.limitOpened = Number.parseInt(this.limitOpened)
+            this.limitOpened = Number.isNaN(this.limitOpened) ? 0 : this.limitOpened;
             if (this.limitOpened !== 0 && this.limitOpened <= this.storage.count) {
+                this.element.remove();
                 return console.log(`Popup has already been opened ${this.limitOpened} times, skipping...`);
+            }
+
+            if (this.element instanceof HTMLDialogElement) {
+                this.initDialog();
+            } else {
+                this.initModal();
             }
 
             setTimeout(() => {
@@ -61,6 +45,65 @@ define([
         },
         onOpened() {
             this.storage.count++;
+
+            if (this.closeTimeout > 0) {
+                this.progressElement = document.createElement('div');
+                this.progressElement.classList.add('progress-timeout');
+                this.progressElement.style.transitionDuration = `${this.closeTimeout}ms`;
+                this.element.parentElement.prepend(this.progressElement);
+
+                this.closeTimeoutHandler = setTimeout(() => {
+                    this.popup.closeModal();
+                }, this.closeTimeout)
+
+                setTimeout(() => {
+                    this.progressElement.classList.add('start');
+                });
+            }
+        },
+        onClose() {
+            clearTimeout(this.closeTimeoutHandler);
+            this.progressElement?.remove();
+        },
+        initDialog() {
+            this.popup = {
+                openModal: () => this.element.showModal(),
+                closeModal: () => this.element.close(),
+            };
+
+            this.element.open = false;
+            this.element.addEventListener('close', this.onClose.bind(this));
+            this.element.addEventListener('cancel', this.onClose.bind(this));
+
+            this.element.querySelectorAll('form[method=dialog] .action-close').forEach(it => {
+                it.addEventListener('click', this.popup.closeModal.bind(this));
+            });
+
+            if (this.modalSettings.clickableOverlay) {
+                const overlayHandler = (event) => {
+                    const dialogRect = this.element.getBoundingClientRect();
+                    const isInWidth = event.x > dialogRect.x && event.x < dialogRect.right;
+                    const isInHeight = event.y > dialogRect.y && event.y < dialogRect.right;
+                    if (isInWidth && isInHeight) {
+                        return;
+                    }
+
+                    this.popup.closeModal();
+                    document.removeEventListener('click', overlayHandler);
+                };
+                document.addEventListener('click', overlayHandler, {passive: true});
+            }
+
+            setTimeout(() => {
+                this.popup.openModal();
+                this.onOpened();
+            }, this.waitTime);
+        },
+        initModal() {
+            const modalSettings = this.modalSettings;
+            modalSettings.opened = this.onOpened.bind(this);
+            modalSettings.closed = this.onClose.bind(this);
+            this.popup = modal(modalSettings, $(this.element));
         }
     })
 })
